8e728b2e07c0671783eaa345dd8b3b1dc6a9c4d5
michaelnavs <michaelnavs@gmail.com>
2021-03-05 19:52:37 -0600

    split pixels into 10x10 patches, need further testing to see if correct

--- main.py
+++ main.py
@@ -9,13 +9,24 @@ from PIL import Image
 
 img = Image.open("./img/20131001_1100.jpg")
 width, height = img.size
-total_pixels = []
 
-for i in range(height):
-    for j in range(width):
-        total_pixels.append(img.getpixel((j, i)))
+total_pixels = []
+for y in range(height):
+    for x in range(width):
+        total_pixels.append(img.getpixel((x, y)))
 
 # print(len(total_pixels)) -> 2500 total pixels
 
-# for pixel in total_pixels:
-#    print(pixel, end=" ")
+for pixel in total_pixels:
+    print(pixel, end=" ")
+
+ten_by_ten_collection = []
+for y in range(0, height, 10):
+    for x in range(0, width, 10):
+        ten_by_ten = []
+        for k in range(100):
+            ten_by_ten.append(total_pixels[x + k + y * width])
+        ten_by_ten_collection.append(ten_by_ten)
+
+for ten_by_ten in ten_by_ten_collection:
+    print(ten_by_ten)
