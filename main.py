e6eb5abe1e23c2964b2db534b1e0917b6b5a4fee
michaelnavs <michaelnavs@gmail.com>
2021-03-07 13:52:50 -0600

    corrected errors in creating 10x10 patches

--- main.py
+++ main.py
@@ -7,26 +7,34 @@ It will then create a 25x300 list of rgb values and save to an excel sheet
 
 from PIL import Image
 
+# create PIL.Image object and store width and height
 img = Image.open("./img/20131001_1100.jpg")
 width, height = img.size
 
-total_pixels = []
+# get rgb values for each pixel in the image
+pixels = []
 for y in range(height):
     for x in range(width):
-        total_pixels.append(img.getpixel((x, y)))
+        pixels.append(img.getpixel((x, y)))
 
-# print(len(total_pixels)) -> 2500 total pixels
+# print(len(pixels)) -> 2500 total pixels
 
-for pixel in total_pixels:
+# print the each pixel value in pixels array
+for pixel in pixels:
     print(pixel, end=" ")
 
+print(end="\n\n")
+
+# create 10x10 patches
 ten_by_ten_collection = []
 for y in range(0, height, 10):
     for x in range(0, width, 10):
         ten_by_ten = []
-        for k in range(100):
-            ten_by_ten.append(total_pixels[x + k + y * width])
+        for i in range(10):
+            for j in range(10):
+                ten_by_ten.append(pixels[x + j + (y + i) * width])
         ten_by_ten_collection.append(ten_by_ten)
 
+# print each 10x10 patch and its respective length
 for ten_by_ten in ten_by_ten_collection:
-    print(ten_by_ten)
+    print(ten_by_ten, len(ten_by_ten), sep=" ", end="\n\n")
