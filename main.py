f4d99c80a577aaa51963b6ad3802cba2f954f29d
michaelnavs <michaelnavs@gmail.com>
2021-03-07 16:07:23 -0600

    save 25x300 rgb values to an excel sheet

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ It will then create a 25x300 list of rgb values and save to an excel sheet
 """
 
 from PIL import Image
+import pandas as pd
 
 # create PIL.Image object and store width and height
 img = Image.open("./img/20131001_1100.jpg")
@@ -26,15 +27,21 @@ for pixel in pixels:
 print(end="\n\n")
 
 # create 10x10 patches
-ten_by_ten_collection = []
+ten_by_ten_list = []
 for y in range(0, height, 10):
     for x in range(0, width, 10):
         ten_by_ten = []
         for i in range(10):
             for j in range(10):
-                ten_by_ten.append(pixels[x + j + (y + i) * width])
-        ten_by_ten_collection.append(ten_by_ten)
+                for k in range(3):
+                    ten_by_ten.append(pixels[x + j + (y + i) * width][k])
+        ten_by_ten_list.append(ten_by_ten)
 
 # print each 10x10 patch and its respective length
-for ten_by_ten in ten_by_ten_collection:
+for ten_by_ten in ten_by_ten_list:
     print(ten_by_ten, len(ten_by_ten), sep=" ", end="\n\n")
+
+ten_by_ten_df = pd.DataFrame(ten_by_ten_list)
+writer = pd.ExcelWriter("test.xlsx", engine="xlsxwriter")
+ten_by_ten_df.to_excel(writer, sheet_name="test", index=False)
+writer.save()
