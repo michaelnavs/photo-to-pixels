e3bb430f3062db83718f413f250217426d76ab6c
michaelnavs <michaelnavs@gmail.com>
2021-03-09 12:51:05 -0600

    implemented main function to loop over files in image directory and create respective dataset for each image

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ It will then create a 25x300 list of rgb values and save to an excel sheet
 from typing import List
 from PIL import Image
 import pandas as pd
+import glob
 
 
 def create_pixel_list(img: Image, width: int, height: int) -> List:
@@ -32,25 +33,25 @@ def create_25_by_300_rgb_values(pixel_array: List, width: int, height: int) -> L
     return ten_by_ten_list
 
 
-def save_25_by_300_rgb_to_excel(ten_by_ten_patch: List, image_name: str) -> None:
-    # saves 25x300 dataset to an excel sheet
+def add_25_by_300_rgb_to_excel(
+    ten_by_ten_patch: List, image_name: str, writer: pd.ExcelWriter
+) -> None:
+    # adds 25x300 dataset to an excel sheet
     ten_by_ten_df = pd.DataFrame(ten_by_ten_patch)
-    writer = pd.ExcelWriter("test.xlsx", engine="xlsxwriter")
     ten_by_ten_df.to_excel(writer, sheet_name=image_name, index=False)
-    writer.save()
 
 
 def main() -> None:
-    pass
-    # create PIL.Image object and store width and height
-    # img = Image.open("./img/20131001_1100.jpg")
-    # width, height = img.size
-
-    # create list of images from images directory
-    # for each image:
-    # create_pixel_list()
-    # create_25_by_300_rgb_values()
-    # save_25_by_300_rgb_to_excel()
+    image_filenames = glob.glob("./images/*.jpg")
+    writer = pd.ExcelWriter("test.xlsx", engine="xlsxwriter")
+    for image_filename in image_filenames:
+        image = Image.open(image_filename)
+        width, height = image.size
+        pixels = create_pixel_list(image, width, height)
+        pixel_patches = create_25_by_300_rgb_values(pixels, width, height)
+        add_25_by_300_rgb_to_excel(pixel_patches, image_filename[9:], writer)
+        print(f"working on pixel dataset for {image_filename}")
+    writer.save()
 
 
 if __name__ == "__main__":
