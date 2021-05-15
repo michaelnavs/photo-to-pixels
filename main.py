c0a551a67f472bc818d3ddc6a92961740a7ba160
michaelnavs <michaelnavs@gmail.com>
2021-05-14 22:54:35 -0500

    setup template for version 3 of the program

---
+++ main.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import pandas as pd
+import glob
+from .utils import (
+    is_black,
+    is_white,
+    is_blue_sky,
+    get_altitude,
+    get_azimuth,
+    get_central_angle,
+    selection_criteria,
+)
+
+
+def main() -> None:
+    image_filenames = glob.glob("./images/*.jpg")
+    # image_filenames.sort() # alaphabetically sort the list of file names
+
+    for image_filename in image_filenames:
+        print(f"{image_filename}...")  # display file name to see progress
+        image = Image.open(image_filename)  # create instance of Pillow.Image
+        width, height = image.size
+        for x in range(width):
+            for y in range(height):
+                pixel = image.getpixel((x, y))  # tuple of RGB values -> (R, G, B)
+                # skip pixels that are pure black, pure white, and are not blue sky pixels
+                if is_black(pixel) or is_white(pixel) or not (is_blue_sky):
+                    continue
+                altitude = get_altitude(x, y)
+                azimuth = get_azimuth(x, y)
+                central_angle = get_central_angle(altitude, azimuth)
+                # if alititude and central angle does not meet the criteria, set RGB value to black
+                if not (selection_criteria(altitude, central_angle)):
+                    image.putpixel((x, y), (0, 0, 0))
+                image.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,47 @@
+def is_black():
+    # slide 14
+    pass
+
+
+def is_white():
+    # slide 14
+    pass
+
+
+def is_blue_sky():
+    # ppt slide 13
+    pass
+
+
+def selection_criteria():
+    pass
+
+
+def get_altitude():
+    # ppt slide 6
+    pass
+
+
+def get_azimuth():
+    # ppt slide 7
+    pass
+
+
+def get_central_angle():
+    # ppt slide 9
+    pass
+
+
+def calculate_rho():
+    # ppt slide 14
+    pass
+
+
+def calculate_theta():
+    # ppt slide 7
+    pass
+
+
+def calculate_psi():
+    # ppt slide 7
+    pass
